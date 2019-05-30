2aa6b0b63363d8a74f93a526172b2dd894841cb2
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-30 17:44:01 -0400

    Add entry point

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/**
+ * Here we mock the event-related `window` methods so that we can listen on
+ * and dispatch events on the server.
+ */
+global.window = {
+  __lib__: {},
+  addEventListener: (eventName, callback) => {
+    global.window.__lib__[eventName] = global.window.__lib__[eventName] || [];
+    global.window.__lib__[eventName].push(callback);
+  },
+  dispatchEvent: (eventName, data) => {
+    global.window.__lib__[eventName] = global.window.__lib__[eventName] || [];
+    global.window.__lib__[eventName].forEach((callback) => callback());
+  },
+};
+
+/**
+ * Then we import the Core and plugin-type modules.
+ */
+const Core = require('./core');
+const DataSource = require('./plugins/data-source');
+const Events = require('./plugins/events');
+
+/**
+ * Finally, we create a new instance of the Core class, passing in
+ * an options object that contains the DataSource and Events plugins.
+ */
+const core = new Core({
+  modules: [
+    DataSource,
+    Events,
+  ],
+});
