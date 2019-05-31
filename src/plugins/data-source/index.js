cc19c3b0dff3e318d2ded9628774cc307d2599e7
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-31 13:57:42 -0400

    Update DataSource to capture opts

--- src/plugins/data-source/index.js
+++ src/plugins/data-source/index.js
@@ -9,6 +9,12 @@ class DataSource extends AbstractPlugin {
     return ['EVENTS'];
   }
 
+  constructor(opts = {}) {
+    super();
+
+    this.opts = opts;
+  }
+
   register() {
     return [
       { listenOn: 'foo', emitOn: 'bar', callback: this.fetch },
