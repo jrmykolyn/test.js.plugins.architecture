b4067a19bf63310a40bc76b87c25fcedfcd37e05
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-31 14:08:15 -0400

    Add AbstractPlugin#resolveSettings()

--- src/core/plugin.js
+++ src/core/plugin.js
@@ -2,6 +2,10 @@ class AbstractPlugin {
   static get DEPENDENCIES() {
     return [];
   }
+
+  resolveSettings(options = {}, defaults = {}) {
+    return Object.assign({}, defaults, options);
+  }
 }
 
 module.exports = AbstractPlugin;
