8c0a284dbf889e453e7d098a438f63a307529d1b
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-31 10:04:25 -0400

    Make DataSource depend on Events

--- src/plugins/data-source/index.js
+++ src/plugins/data-source/index.js
@@ -4,6 +4,10 @@ class DataSource extends AbstractPlugin {
   static get TYPE() {
     return 'DATA_SOURCE';
   }
+
+  static get DEPENDENCIES() {
+    return ['EVENTS'];
+  }
 }
 
 module.exports = DataSource;
