aa6364e17b289ef97dbcd3b7185a9f82301cf72e
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-06-05 09:25:58 -0400

    Update index.js: prepare for browser deployment

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 /**
- * Here we mock the event-related `window` methods so that we can listen on
- * and dispatch events on the server.
- */
-global.window = {
-  __lib__: {},
-  addEventListener: (eventName, callback) => {
-    global.window.__lib__[eventName] = global.window.__lib__[eventName] || [];
-    global.window.__lib__[eventName].push(callback);
-  },
-  dispatchEvent: (eventName, data) => {
-    global.window.__lib__[eventName] = global.window.__lib__[eventName] || [];
-    global.window.__lib__[eventName].forEach((callback) => callback(data));
-  },
-  CustomEvent: function(data = {}) {
-    return data;
-  },
-};
-
-/**
- * Then we import the Core and plugin-type modules.
+ * First we import the Core and plugin-type modules.
  */
 const Core = require('./core');
 const DataSource = require('./plugins/data-source');
@@ -30,8 +11,11 @@ const Cache = require('./plugins/cache');
  * After importing our plugin classes, we create a new instance of the Core class,
  * passing in an options object that contains the DataSource, Events, Filter,
  * and Cache plugins.
+ *
+ * We also expose the Core instance via the `__CORE__` property of the `window`
+ * object.
  */
-const core = new Core({
+const core = window.__CORE__ = new Core({
   modules: [
     [
       DataSource,
@@ -44,21 +28,4 @@ const core = new Core({
     Filter,
     Cache,
   ],
-});
-
-/**
- * Finally, we register and dispatch a series of events to demonstrate that
- * the Logic layer is working as expected.
- */
-const REGISTER = ['bar', 'baz', 'quux'];
-const DISPATCH = ['foo', 'bar', 'baz'];
-
-REGISTER.forEach((eventName) => {
-  window.addEventListener(eventName, (e) => {
-    console.log(`__ RECEIVED ${eventName}`, e);
-  });
-});
-
-DISPATCH.forEach((eventName) => {
-  window.dispatchEvent(eventName, {});
 });
