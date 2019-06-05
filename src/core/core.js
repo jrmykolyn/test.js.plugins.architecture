bb53e3dcd40455dc959f03f45715eb77f27c38db
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-06-05 16:27:04 -0400

    Add comments to index.js

--- src/core/core.js
+++ src/core/core.js
@@ -1,30 +1,71 @@
 const { PluginTypes } = require('./utils');
 
+/**
+ * The Core class is responsible for instantiating plugins,
+ * facilitating communication between them, and wrapping select
+ * plugin methods such that they may be accessed by sibling
+ * plugins.
+ */
 class Core {
+  /**
+   * Core exposes a series of static getter properties. The `ORDER`
+   * property returns an array of plugin types, which is used to
+   * ensure that the plugins are instantiated in the correct order.
+   */
   static get ORDER() {
     return [
-      'FILTER',
-      'DATA_SOURCE',
-      'EVENTS',
+      PluginTypes.FILTER,
+      PluginTypes.DATA_SOURCE,
+      PluginTypes.EVENTS,
+      PluginTypes.CACHE,
     ];
   }
 
+  /**
+   * At instantiation time, Core is provided with an options object
+   * that contains all of the data and configuration options that
+   * are required for the current session.
+   */
   constructor(opts = {}) {
     const { modules = [] } = opts;
 
-    // Instantiate modules.
+    /**
+     * The primary role of the constructor function is to instantiate any
+     * plugins received via the options object. Core delegates this responsibility
+     * to the `instantiateModules()` method, which receives an array of
+     * modules as its sole argument. This method returns an array of module
+     * instances, which are stored as the `modules` instance property.
+     */
     this.modules = this.instantiateModules(modules);
 
-    // Register event listeners.
+    /**
+     * Additionally, the Core instance is responsible for facilitating
+     * communication between modules, including the `DATA_SOURCE` and
+     * `EVENTS`modules.
+     *
+     * `DATA_SOURCE` plugins expose a `register()` method, which returns
+     * an array of objects, each of which contains an event to 'listen'
+     * on, an event to 'emit' on, and a callback to invoke when the former
+     * event is 'heard'.
+     *
+     * `EVENTS` plugins also expose a `register()` method. This method
+     * accepts the data returned by the `DATA_SOURCE` plugins' `register()`,
+     * and registers a environment-specific event listeners.
+     */
     const [eventsModules] = this.getModulesByType(PluginTypes.EVENTS);
     const dataSourceModules = this.getModulesByType(PluginTypes.DATA_SOURCE);
     dataSourceModules.forEach((moduleInstance) => {
       moduleInstance.register().forEach(({ listenOn, emitOn, callback }) => {
-        eventsModules.register(listenOn, emitOn, callback);
+        eventsModule.register(listenOn, emitOn, callback);
       });
     });
   }
 
+  /**
+   * The `*Cache()` methods are thin wrappers around a `CACHE`-type plugin,
+   * each of which checks for the presence of a `CACHE` plugin before
+   * invoking the corresponding method.
+   */
   hasCache(key) {
     const [cache] = this.getModulesByType(PluginTypes.CACHE);
     if (cache) return cache.has(key);
@@ -40,6 +81,19 @@ class Core {
     if (cache) return cache.put(key, value);
   }
 
+  /**
+   * The `applyFilters()` method is responsible for transforming event-related
+   * payloads before they are provided as arguments to the corresponding callback
+   * functions.
+   *
+   * In cases where the Core instance contains one or more `FILTER`-type plugins,
+   * each plugin's `filter()` method will be invoked with the payload data as its
+   * sole argument. In these cases, the `FILTER`-type plugins will be applied in
+   * the same order that they were provided at Core instantiation time.
+   *
+   * If the Core instance does not include any `FILTER`-type plugins, the event
+   * payload will not be modified.
+   */
   applyFilters(listenOn, emitOn, data) {
     const filters = this.getModulesByType(PluginTypes.FILTER);
     const payload = { listenOn, emitOn, data };
@@ -50,6 +104,15 @@ class Core {
       : payload;
   }
 
+  /**
+   * The `instantiateModules()` method is responsible for instantiating each
+   * plugin received by Core via the options object. In order to resolve
+   * dependencies, plugin classes are sorted prior to instantiation.
+   *
+   * This method also defines an array of plugin types, which are extracted
+   * from the plugin classes themselves. This array is passed to the underlying
+   * `instantiateModule()` method.
+   */
   instantiateModules(modules) {
     const moduleTypes = this.getModuleTypes(modules);
     return this.sortModulesByType(modules).map((module) => {
@@ -57,6 +120,9 @@ class Core {
     });
   }
 
+  /** The `instantiateModule()` method is responsible for instantiating a
+   * single plugin, which it receives alongside an array of valid plugin types.
+   */
   instantiateModule(module, types = []) {
     const isArr = Array.isArray(module);
     const mod = isArr ? module[0] : module;
@@ -70,14 +136,26 @@ class Core {
     return new mod(this, modOpts);
   }
 
+  /**
+   * The `getModuleTypes()` utility method extracts the plugin type from each plugin
+   * provided via the `modules` array.
+   */
   getModuleTypes(modules) {
     return modules.map((module) => module.TYPE).filter((type, i, arr) => i === arr.indexOf(type));
   }
 
+  /**
+   * The `getModulesByType()` utility method returns all plugins that have been instantiated,
+   * and which match the type provided.
+   */
   getModulesByType(type) {
     return this.modules.filter((module) => module.constructor.TYPE === type);
   }
 
+  /**
+   * The `sortModulesByType()` utility method returns an array of plugins sorted by type.
+   * The sorting criteria is defined by the `ORDER` property of the Core class.
+   */
   sortModulesByType(modules) {
     return modules.slice(0).sort((a, b) => {
       // TODO: Refactor.
--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 /**
- * First we import the Core and plugin-type modules.
+ * This file represents the entry point for the Webpack process, the output
+ * of which is a JavaScript artifact that is loaded via a <script> tag.
+ */
+
+/**
+ * First we import the Core module, the plugins, and our dictionary of
+ * Web Component classes.
  */
 const { Core } = require('./core');
 const DataSource = require('./plugins/data-source');
 const Events = require('./plugins/events');
 const Filter = require('./plugins/filter');
 const Cache = require('./plugins/cache');
+const { Components }  = require('./ui');
 
 /**
- * Then we import and expose our dictionary of Web Components.
- */
-const Components = window.__COMPONENTS__ = require('./ui').Components;
-
-/**
- * After importing our plugin classes, we create a new instance of the Core class,
- * passing in an options object that contains the DataSource, Events, Filter,
- * and Cache plugins.
- *
- * We also expose the Core instance via the `__CORE__` property of the `window`
+ * Since the demo page will be responsible for registering each of the
+ * Web Components, we expose the dictionary as a property of the `window`
  * object.
+ */
+window.__COMPONENTS__ = Components;
+
+/**
+ * Here we create a new instance of the Core class, passing in an options object,
+ * that contains our plugin classes. We also expose the Core instance via the
+ * `__CORE__` property of the `window` object.
+ *
+ * For plugins that do not require configuration options, the plugin class is
+ * sufficient. In cases where a plugin either requires or accepts configuration
+ * options, these may be provided using an array-types structure. In this case,
+ * Core expects the first member of the array to be the plugin class, and the
+ * second member to be an options object.
  */
 const core = window.__CORE__ = new Core({
   modules: [
