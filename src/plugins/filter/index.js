4e209c0ae4218ff6956bfec004d18f53ed67aa99
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-06-05 08:25:47 -0400

    Update Filter: do not set 'done' prop

--- src/plugins/filter/index.js
+++ src/plugins/filter/index.js
@@ -4,7 +4,7 @@ class Filter extends AbstractFilterPlugin {
   filter(payload) {
     const { emitOn, data } = payload;
     return emitOn === 'quux'
-      ? { ...payload, data: data.toUpperCase(), done: true }
+      ? { ...payload, data: data.toUpperCase() }
       : payload;
   }
 }
